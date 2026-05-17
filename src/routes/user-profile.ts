0b6dbd347fc889fa96b1c90ef8874a6c6da55992
himxnshu <khatri.hk18@gmail.com>
2026-05-17 16:01:16 +0530

    Add user profile hash endpoints for creating and retrieving user profiles and required bruno api test files

--- src/routes/user-profile.ts
+++ src/routes/user-profile.ts
@@ -50,5 +50,36 @@ export const userProfileRouter = (): Router => {
             }
         })
 
+    router.route('/user/:id/hash')
+        .post(async (req: Request, res: Response) => {
+            try {
+                const { id } = req.params;
+                if (!req.body) {
+                    return res.json({
+                        msg: "request body should not be empty"
+                    })
+                }
+                await redis.hset(`user:${id}:hash`, req.body)
+                res.json({ savedAs: "hash" })
+            } catch (err) {
+                res.status(500).json({
+                    error: err instanceof Error ? {
+                        message: err.message,
+                        stack: err.stack
+                    } : "An unknown error occurred"
+                })
+            }
+        })
+        .get(async (req: Request, res: Response) => {
+            const { id } = req.params;
+            const user = await redis.hgetall(`user:${id}:hash`)
+            if (!user || Object.keys(user).length === 0) {
+                return res.status(400).json({
+                    msg: "user profile not found!"
+                })
+            }
+
+            res.json({ user })
+        })
     return router;
 }
